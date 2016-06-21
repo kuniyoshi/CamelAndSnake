3cc0553343483c5e3194346896309f0ab59eb804
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2016-06-21 09:03:18 +0900

    Try digit scroll

---
+++ Assets/Scripts/Toybox/CameraMover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Toybox
+{
+
+public class CameraMover : MonoBehaviour
+{
+
+	public float y;
+
+	Canvas canvas;
+	Camera scrollCamera;
+
+	void Awake()
+	{
+		canvas = GetComponentInChildren<Canvas> ();
+		Debug.Assert (canvas);
+		scrollCamera = GetComponent<Camera> ();
+		Debug.Assert (scrollCamera);
+	}
+
+	void Start()
+	{
+		canvas.renderMode = RenderMode.WorldSpace;
+		Vector3 pos = canvas.transform.position;
+		pos.z = 1.9f;
+		canvas.transform.position = pos;
+
+		scrollCamera.fieldOfView = 38f;
+	}
+
+	void Update()
+	{
+		// -5.3f;
+
+		float minY = -5.3f;
+		float maxY = 5.3f;
+
+		float currentY = (maxY - minY) * y + minY;
+		Vector3 pos = canvas.transform.position;
+		pos.y = currentY;
+		canvas.transform.position = pos;
+	}
+
+}
+
+}
