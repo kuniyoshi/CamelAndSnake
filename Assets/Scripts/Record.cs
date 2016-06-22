998d2470ef2a05a65413383ff5b042df2187d847
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2016-06-22 20:24:36 +0900

    Add Record to template

--- Assets/Scripts/Record.cs
+++ Assets/Scripts/Record.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+// *****************************************************
+// *** This file will be baked from ScriptTemplates. ***
+// *****************************************************
+
 public class Record
 {
 
@@ -8,25 +12,33 @@ public class Record
 
 	public int CamelCount { get; set; }
 
+	public int CamelWordCount { get; set; }
+
 	public float CamelTotalTime { get; set; }
 
 	public int SnakeCount { get; set; }
+
+	public int SnakeWordCount { get; set; }
 
 	public float SnakeTotalTime { get; set; }
 
 	public void Save()
 	{
 		PlayerPrefs.SetInt (Prefix + "CamelCount", CamelCount);
+		PlayerPrefs.SetInt (Prefix + "CamelWordCount", CamelWordCount);
 		PlayerPrefs.SetFloat (Prefix + "CamelTotalTime", CamelTotalTime);
 		PlayerPrefs.SetInt (Prefix + "SnakeCount", SnakeCount);
+		PlayerPrefs.SetInt (Prefix + "SnakeWordCount", SnakeWordCount);
 		PlayerPrefs.SetFloat (Prefix + "SnakeTotalTime", SnakeTotalTime);
 	}
 
 	public void Load()
 	{
 		CamelCount = PlayerPrefs.GetInt (Prefix + "CamelCount");
+		CamelWordCount = PlayerPrefs.GetInt (Prefix + "CamelWordCount");
 		CamelTotalTime = PlayerPrefs.GetFloat (Prefix + "CamelTotalTime");
 		SnakeCount = PlayerPrefs.GetInt (Prefix + "SnakeCount");
+		SnakeWordCount = PlayerPrefs.GetInt (Prefix + "SnakeWordCount");
 		SnakeTotalTime = PlayerPrefs.GetFloat (Prefix + "SnakeTotalTime");
 	}
 
---
+++ ScriptTemplates/Record.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[%- # CARE BOM EXISTENCE -%]
+[% SET fields => [
+	{ type => "int", name => "CamelCount" },
+	{ type => "int", name => "CamelWordCount" },
+	{ type => "float", name => "CamelTotalTime" },
+	{ type => "int", name => "SnakeCount" },
+	{ type => "int", name => "SnakeWordCount" },
+	{ type => "float", name => "SnakeTotalTime" },
+] -%]
+
+public class Record
+{
+
+	static string Prefix = "Record.";
+
+[%- FOREACH field IN fields -%]
+
+	public [% GET field.type %] [% GET field.name %] { get; set; }
+[% END # FOREACH field IN fields -%]
+
+	public void Save()
+	{
+[% FOREACH field IN fields -%]
+		PlayerPrefs.Set[% GET field.type.ucfirst -%] (Prefix + "[% GET field.name %]", [% GET field.name %]);
+[% END # FOREACH field IN fields -%]
+	}
+
+	public void Load()
+	{
+[% FOREACH field IN fields -%]
+		[% field.name %] = PlayerPrefs.Get[% field.type.ucfirst %] (Prefix + "[% GET field.name %]");
+[% END # FOREACH field IN fields -%]
+	}
+
+}
